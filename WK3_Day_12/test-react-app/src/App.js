5321bde3337e154e838687559858c1c3a4d2510e
le8025 <e0838431@u.nus.edu>
2022-09-20 20:11:01 +0800

    Day 12 submission

---
+++ WK3_Day_12/test-react-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+/* Task 2. Create the React app, edit the App.js and run.
+    - any element is okay.
+*/
+
+function App() {
+  return (
+    <>
+      <h1 className='red'>Hello!</h1>
+      <h2>This is a React App.</h2>
+    </>
+  );
+}
+
+export default App;
